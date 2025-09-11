c36a6c165a9f268bdce08d53d697b1c4b11601b0
dericmartinssenaisc <deric.martins@edu.sc.senai.br>
2025-09-11 20:39:06 -0300

    Operação de comparação

--- script.js
+++ script.js
@@ -1,19 +1,27 @@
-/*
-    Em portugol:
-    inteiro nota = 85
+{
+//compração simples de tipos
+let a = 5;
+let b = "5";
 
-    se (nota >= 90){
-        escreva("Aprovado")
-
-    }senao {
-        escreva("Reprovado")
-    }
-*/
-
-let nota = 95
-
-if (nota >= 90) {
-    console.log("Aprovado");
-}else{
-    console.log("Reprovado");   
+// conversão automática de tipos
+if (a = b) {
+    console.log("true");
+} else {
+    console.log("false");
+}   
 }
+
+
+{
+  //compração estrita de tipos  
+    let a = 5;
+    let b = "5";
+    
+    // conversão automática de tipos
+    if (a === b) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }   
+}
+    
