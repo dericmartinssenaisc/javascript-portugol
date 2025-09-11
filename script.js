6bed8082500c29f0c2b5b68a6cbcc7d4c03c90f8
dericmartinssenaisc <deric.martins@edu.sc.senai.br>
2025-09-11 19:20:40 -0300

    Variaveis

---
+++ script.js
@@ -0,0 +1,11 @@
+let nome = "Deric";
+let idade = 19;
+let cidade = "Alta Floresta - MT";
+let altura = 1.69;
+let ehHumano = true;
+
+console.log("Nome: " + nome);
+console.log("Idade: " + idade);
+console.log("Cidade: " + cidade);
+console.log("Altura: " + altura + "m");
+console.log("É humano? " + ehHumano);
