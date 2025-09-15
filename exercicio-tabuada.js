1340e40b6fa1882e54e5959772d6b50c7c00f4b6
dericmartinssenaisc <deric.martins@edu.sc.senai.br>
2025-09-15 20:27:54 -0300

    Codigo exercicio tabuada

---
+++ exercicio-tabuada.js
@@ -0,0 +1,10 @@
+let numero = 5;
+
+console.log(`=== Tabuada do ${numero} ===`);
+
+for (let i = 1; i <= 10; i++) {
+    let resultado = numero * i;
+    console.log(`${numero} x ${i} = ${resultado}`); 
+}
+
+// Fim do código    
---
+++ teste.js
@@ -0,0 +1,20 @@
+let numero = 8;
+if (numero % 2 === 0) {
+  console.log("Número par");
+}
+
+
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+  } 
+
+  for (let i = 1; i < 5; i++) {
+    console.log(i);
+  }
+  
+
+  let i = 1;
+while (i <= 5) {
+  console.log(i);
+  i++;
+} 
