c5dcf66ff60845c9b03d702aa14762808706fe45
dericmartinssenaisc <deric.martins@edu.sc.senai.br>
2025-09-15 20:54:42 -0300

    Update script references and add tabuada functionality

---
+++ funcao.js
@@ -0,0 +1,39 @@
+/*
+    Função em Portugol
+
+    função real calculadoraMedia(real n1, real n2) {
+        retorne (n1 + n2) / 2
+    }
+
+*/
+
+function calculadoraMedia(n1, n2) {
+    return (n1 + n2) / 2
+}
+
+function adicao(n1, n2) {
+    return n1 + n2
+}
+
+function sub(n1, n2) {
+    return n1 - n2
+}
+
+function mult(n1, n2) {
+    return n1 * n2
+}
+
+function divisao(n1, n2) {
+    return n1 / n2
+}
+
+let resultado = sub(7, 8);
+
+console.log(`A média é: ${resultado}`);
+
+
+
+
+
+
+
